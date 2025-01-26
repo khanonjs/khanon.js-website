50861493a20c1a7731f16566435de2c317b7f1df
development@khanon3d.com <development@khanon3d.com>
2025-01-26 14:49:55 +0100

    Remove unused documentation files and update version to 0.2.32; add Babylon.js logo to main buttons

--- src/components/main-buttons/main-buttons.tsx
+++ src/components/main-buttons/main-buttons.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import babylonLogo from '../../assets/babylonjs_identity_color.png'
 import githubButton from '../../assets/github-button-gray.png'
 import playCircle from '../../assets/play-circle.svg'
 import { ElementStyle } from '../../classes/element-style'
@@ -8,6 +9,12 @@ import styles from './main-buttons.module.scss'
 import { MainButtonsProps } from './main-buttons.props'
 
 export class MainButtons extends React.Component<MainButtonsProps> {
+
+  refBabylon() {
+
+  }
+
+
   handlePlayDemos() {
     // this.props.cbSetPage(Pages.GET_STARTED)
   }
@@ -18,6 +25,10 @@ export class MainButtons extends React.Component<MainButtonsProps> {
 
   handleGithub() {
     window.open('https://github.com/khanonjs/khanon.js', '_blank', 'noreferrer')
+  }
+
+  handleBabylon() {
+    window.open('https://www.babylonjs.com/', '_blank', 'noreferrer')
   }
 
   render() {
@@ -47,6 +58,16 @@ export class MainButtons extends React.Component<MainButtonsProps> {
           <img
             src={githubButton}
             className={styles['main-buttons-github-img']}
+          />
+        </div>
+        <div
+          className={ElementStyle.getClass(styles, ['babylon-logo', 'rsp-babylon-logo-step2'])}
+          ref={this.refBabylon.bind(this)}
+        >
+          <img
+            src={babylonLogo}
+            className={ElementStyle.getClass(styles, ['babylon-logo-img', 'rsp-babylon-logo-img-step1', 'rsp-babylon-logo-img-step2'])}
+            onClick={this.handleBabylon.bind(this)}
           />
         </div>
       </div>
