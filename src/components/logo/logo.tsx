f0f03f712743d1bc486fc75e973ae56aafb6d735
development@khanon3d.com <development@khanon3d.com>
2025-03-31 02:04:36 +0200

    .

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -8,7 +8,7 @@ export class Logo extends React.Component {
     return (
       <div className={ElementStyle.getClass(styles, ['logo-container', 'rsp-logo-container'])}>
         <div className={ElementStyle.getClass(styles, ['logo-header', 'font-luckiestguy-regular'])}>KHANON</div>
-        <div className={ElementStyle.getClass(styles, ['logo-footer', 'font-roadgeek-regular'])}>A typescript games engine extending Babylon.js (ALPHA)</div>
+        <div className={ElementStyle.getClass(styles, ['logo-footer', 'font-roadgeek-regular'])}>A typescript game engine extending Babylon.js (ALPHA)</div>
       </div>
     )
   }
